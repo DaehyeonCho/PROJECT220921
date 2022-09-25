8aaaf1a30d95c08c0b9e783cf0a169e86ac27924
wheogus4629 <69081057+wheogus4629@users.noreply.github.com>
2022-09-25 18:31:45 +0900

    Create Challenge12.java

---
+++ Challenge12.java
@@ -0,0 +1,17 @@
+public class practice1 {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i = 0; i < args.length; i++) {
+			try {
+				int n = Integer.parseInt(args[i]);
+				sum = sum + n;
+			}
+			catch(NumberFormatException e) {}
+		}
+		
+		System.out.println(sum);
+	}
+
+}
